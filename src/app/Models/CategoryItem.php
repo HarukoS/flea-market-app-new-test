9bc1a86f5da7f162d9bb3586d1be9df0697d7025
hsetogawa <togaharu314@gmail.com>
2025-10-26 10:53:06 +0900

    CSS、シーダーファイル修正

--- src/app/Models/CategoryItem.php
+++ src/app/Models/CategoryItem.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category_item extends Model
+class CategoryItem extends Model
 {
     use HasFactory;
 
--- src/database/seeders/UsersTableSeeder.php
+++ src/database/seeders/UsersTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,6 +14,22 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $param = [
+            'name' => '鈴木太郎',
+            'email' => 'user1@gmail.com',
+            'email_verified_at' => '2025-10-25 00:00:00',
+            'created_at' => '2025-10-25 00:00:00',
+            'password' => bcrypt('password'),
+        ];
+        DB::table('users')->insert($param);
+
+        $param = [
+            'name' => '鈴木花子',
+            'email' => 'user2@gmail.com',
+            'email_verified_at' => '2025-10-25 00:00:00',
+            'created_at' => '2025-10-25 00:00:00',
+            'password' => bcrypt('password'),
+        ];
+        DB::table('users')->insert($param);
     }
 }
